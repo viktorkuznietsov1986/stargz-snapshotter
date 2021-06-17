1f74b712b7d7e379762a9789c96ad4c02c136f9e
Kuznietsov <vkuzniet@88665a036e00.ant.amazon.com>
2021-06-17 17:14:45 -0400

    Fixed the metrics

--- fs/fs.go
+++ fs/fs.go
@@ -110,10 +110,10 @@ func NewFilesystem(root string, cfg config.Config, opts ...Option) (_ snapshot.F
 	}
 
 	var ns *metrics.Namespace
-	var fsMetrics *fsmetrics.FileSystemMetrics
 	if !cfg.NoPrometheus {
 		ns = metrics.NewNamespace("stargz", "fs", metrics.Labels{"handler": "metrics"})
-		fsmetrics.Register()
+		
+		fsmetrics.Register() // Register duration metrics. This will happen only once.
 	}
 	c := fsmetrics.NewLayerMetrics(ns)
 	if ns != nil {
